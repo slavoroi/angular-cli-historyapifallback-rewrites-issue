80ec6fa9a764cbb7923bb60a0e6a3b8cd841bd41
Slavo <slavoroikhel@gmail.com>
2019-09-02 17:50:38 +0300

    Add files via upload

---
+++ src/app-routing.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {TestmeComponent} from './testme/testme.component';
+
+
+const routes: Routes = [
+  {path: 'trax-one/user-management/auth', component: TestmeComponent}/*,
+  {
+    path: 'features-modules',
+    loadChildren: () => import('app/features-modules/features-modules.module').then(m => m.FeaturesModulesModule)
+  }*/
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
